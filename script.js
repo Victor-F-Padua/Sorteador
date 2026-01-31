13c5e2ff14d5e0ff59abb1fac55d37b3fd1686d6
Victor Ferreira <victor.f.padua@gmail.com>
2026-01-31 14:34:26 -0300

    adding if and else

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function runCode() {
 
     const min = Math.ceil(document.querySelector('.minimo').value)
     const max = Math.floor(document.querySelector('.maximo').value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min
+
+   
     
-    showValue.innerHTML = result
+    if(max > min) {
+         const result = Math.floor(Math.random() * (max - min + 1)) + min
+         showValue.innerHTML = result
+        } else {
+            alert("The maximum value needs to be higher.")
+        }
+    
 
-
+    
 }
 
 
